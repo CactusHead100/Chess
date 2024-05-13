2fde068b09671eb0dd30c9d7f1c3621a3adcf7a8
CactusHead100 <cactushead100@gmail.com>
2024-05-14 06:30:14 +1200

    Managed to get a rectangle drawing

--- Main/Drawing.java
+++ Main/Drawing.java
@@ -3,12 +3,15 @@ package Main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JPanel;
 
 public class Drawing extends JPanel{
     private Mouse_Input mouse_Input = new Mouse_Input(this);
-    
+    Rectangle2D.Double rectangle = new Rectangle.Double(0,0,100,100);
+
     public Drawing(){
         addMouseListener(mouse_Input);
         addMouseMotionListener(mouse_Input);
@@ -16,11 +19,13 @@ public class Drawing extends JPanel{
     
     public void MouseClicked(){
         System.out.println("Click");
+        repaint();
     }
 
-    public void paintcomponent(Graphics graphics){
-        super.paint(graphics);
-        Graphics graphics2d = (Graphics2D) graphics;
-        
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.black);
+        g2d.fill(rectangle); 
     }
 }
